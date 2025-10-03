c3e37e5b1cf10fb5f3e19774de5e6029140561b6
Jorge Zapata <54174180+jzapataa@users.noreply.github.com>
2025-10-03 14:10:48 +0200

    Update _app.tsx
    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,5 +3,19 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return(
+    <>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LWFTZ058XY"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-LWFTZ058XY');
+    </script>
+    
+  
+  <Component {...pageProps} />;
+    </>
+      );
 }
