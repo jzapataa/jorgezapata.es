221dbf696274d2718c7d6e078030e8e1c70fbfb8
Jorge Zapata <54174180+jzapataa@users.noreply.github.com>
2025-10-03 14:21:35 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,22 @@ import type { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
   return(
     <>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LWFTZ058XY"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'G-LWFTZ058XY');
-    </script>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id==G-LWFTZ058XY`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XXXXXXXXXX');
+        `}
+      </Script>
     
   
   <Component {...pageProps} />;
